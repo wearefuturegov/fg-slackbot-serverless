57bcf0fa17aec4691650edf58c3aaac54b90924d
userman123 <dominic.freeman123@gmail.com>
2020-09-11 17:45:34 +0100

    We maade it work

--- handler.js
+++ handler.js
@@ -1,19 +1,99 @@
 'use strict'
 
-const { WebClient } = require('@slack/web-api')
+require("dotenv").config()
+const { WebClient, LogLevel } = require('@slack/web-api')
 
-const web = new WebClient(process.env.SLACK_TOKEN)
+const web = new WebClient(process.env.OAUTH_ACCESS_TOKEN, {
+  logLevel: LogLevel.DEBUG
+})
+
+// C024ZUHHC = #development channel
+// C024ZJU25 = #general channel
+const channelID = 'C024ZJU25'
 
 module.exports.alert = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
+  let parsedBody = JSON.parse(event.body)
+
+  console.log(parsedBody)
+
+  if(parsedBody.type === "url_verification"){
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        challenge: parsedBody.challenge
+      })
+    }
+  } else if(parsedBody.type === "event_callback"){
+
+    let { id, creator } = parsedBody.event.channel
+    const conversationResponse = await web.conversations.info({ 
+      channel: parsedBody.event.channel.id
+    })
+    
+    
+    const images = ['https://media.giphy.com/media/3o6MbsOVFTUYBZTCZG/giphy.gif', 'https://media.giphy.com/media/pNiDRNtb9yWdi/giphy.gif', 'https://media.giphy.com/media/PpMeUUFlZXQQ0/giphy.gif']
+    let randomNum = Math.floor(Math.random() * Math.floor(images.length));
+
+
+    let blocks = [
       {
-        message: 'Example response here',
-        input: event,
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": ":sparkles: ✧･ﾟ: *✧･ﾟ:* *New channel created* *:･ﾟ✧*:･ﾟ✧ :sparkles: "
+        }
       },
-      null,
-      2
-    ),
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `*Channel:*\n<#${id}>`
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `*Created by:*\n<@${creator}>`
+        }
+      }
+    ];
+
+
+    if(conversationResponse.channel.purpose.value) {
+      blocks.push({
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `*Description:*\n${conversationResponse.channel.purpose.value}`
+        }
+      })
+    }
+  
+  
+    // blocks.push({
+    //   "type": "image",
+    //   "image_url": images[randomNum],
+    //   "alt_text": "gif"
+    // })
+
+
+
+    const res = await web.chat.postMessage({ 
+      channel: channelID, 
+      text: `:satellite_antenna: New channel created: <#${id}> by: <@${creator}>\nDescription: \n"_${conversationResponse.channel.purpose.value || "None given"}_"`,
+      blocks: blocks
+    })
+
+
+    return {
+      statusCode: 200,
+      body: "Notified successfully"
+    }
+  }
+
+  return {
+    statusCode: 500,
+    body: "Something weird happened "
   }
 }
